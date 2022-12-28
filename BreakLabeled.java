6dcea882a82ef65d1c14883f821bd23f5c6bf533
Luis Gabriel Oliveira <97142989+luisgabriel1@users.noreply.github.com>
2022-12-28 10:49:09 -0300

    Update BreakLabeled.java

--- BreakLabeled.java
+++ BreakLabeled.java
@@ -2,6 +2,9 @@ package Udemy;
 
 public class BreakLabeled {
     public static void main(String[] args) {
+        
+        // Here below i using two ties for one inside the other. And using the break for stop ties extern.
+        
         Extern: for(int i = 0; i < 3; i++){
             Intern: for(int j = 0; j < 3; j++){
                 if (i == 1){
