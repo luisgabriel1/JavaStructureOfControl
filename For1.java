c6766fe6d695e2be0a0e42c6c312bee4fd137eed
Luis Gabriel Oliveira <97142989+luisgabriel1@users.noreply.github.com>
2022-12-28 10:43:10 -0300

    Update For1.java

--- For1.java
+++ For1.java
@@ -2,7 +2,8 @@ package Udemy;
 
 public class For1 {
     public static void main(String[] args) {
-
+        // Here below i using the ties for.
+        
         for (int counter = 1; counter <= 10; counter++){
 
             System.out.println(counter);
